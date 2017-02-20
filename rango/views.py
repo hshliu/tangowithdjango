11ca27b62a72f88a1f0fe1b3a75f44452146a58b
hshliu <hongshunliu@hotmail.com>
2017-02-21 00:29:23 +0800

    create the static and media files

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,15 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    #sContent = "Rango says hey there partner! <br/>\
+    #        <a href='/rango/about/'>About</a>" 
+    #return HttpResponse(sContent)
+    context_dict = {'boldmessage':
+            "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context=context_dict)    
+
+def about(request):
+    context_dict = {'myname':'Hongshun Liu'}
+    return render(request, 'rango/about.html', context=context_dict)
